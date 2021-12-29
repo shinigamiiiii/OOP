57d1118d6d0893ecc25f2d387abe12b59af4bac3
shinigamiiiii <75800331+shinigamiiiii@users.noreply.github.com>
2021-12-29 23:28:31 +0300

    Рефлексия

---
+++ laba12/laba12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace laba12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "laba12.Product";
+            using (StreamWriter sw = new StreamWriter(@"text.txt", false, Encoding.Default))
+            {
+                sw.WriteLine(Reflector.GetAssemblyName());
+                sw.WriteLine("-----------");
+                sw.WriteLine(Reflector.HasPublicConstructors(name));
+                sw.WriteLine("-----------");
+
+                Reflector.PublicMethods(name, sw);
+                sw.WriteLine("-----------");
+                Reflector.FieldsAndProperties(name, sw);
+                sw.WriteLine("-----------");
+                Reflector.Interfaces(name, sw);
+                sw.WriteLine("-----------");
+                Reflector.MethodType(name, "newPrice", sw); 
+                sw.WriteLine("-----------");
+                Reflector.Invoke(name, "Print");
+                sw.WriteLine(Reflector.Create(name).ToString());
+                sw.WriteLine("-----------");
+                sw.WriteLine("-----------");
+                sw.WriteLine("-----------");
+            }
+            name = "System.String";
+            using (StreamWriter sw = new StreamWriter(@"text.txt", true, Encoding.Default))
+            {
+                sw.WriteLine(Reflector.GetAssemblyName());
+                sw.WriteLine("-----------");
+                sw.WriteLine(Reflector.HasPublicConstructors(name));
+                sw.WriteLine("-----------");
+
+                Reflector.PublicMethods(name, sw);
+                sw.WriteLine("-----------");
+                Reflector.FieldsAndProperties(name, sw);
+                sw.WriteLine("-----------");
+                Reflector.Interfaces(name, sw);
+                sw.WriteLine("-----------");
+                Reflector.MethodType(name, "value", sw);
+            }
+        }
+    }
+}
---
+++ laba12/laba12/Reflector.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba12
+{
+    static class Reflector
+    {
+        public static string GetAssemblyName()
+        {
+            return typeof(Program).Assembly.GetName().FullName;
+        }
+        public static bool HasPublicConstructors(string name)
+        {
+            Type type = Type.GetType(name, false, true);
+            foreach (var item in type.GetConstructors())
+            {
+                if (item.IsPublic)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static void PublicMethods(string name, StreamWriter sw)
+        {
+            Type type = Type.GetType(name, false, true);
+            foreach (MethodInfo method in type.GetMethods())
+            {
+                if (method.IsPublic)
+                {
+                    sw.WriteLine(method.Name);
+                }
+            }
+        }
+        public static void FieldsAndProperties(string name, StreamWriter sw)
+        {
+            Type type = Type.GetType(name, false, true);
+            foreach (var item in type.GetFields())
+            {
+                sw.WriteLine(item.Name);
+            }
+            foreach (var item in type.GetProperties())
+            {
+                sw.WriteLine(item.Name);
+            }
+        }
+        public static void Interfaces(string name, StreamWriter sw)
+        {
+            Type type = Type.GetType(name, false, true);
+            foreach (var item in type.GetInterfaces())
+            {
+                sw.WriteLine(item.Name);
+            }
+        }
+        public static void MethodType(string name, string param, StreamWriter sw)
+        {
+            Type type = Type.GetType(name, false, true);
+            foreach (var item in type.GetMethods())
+            {
+                if (item.GetParameters().Any(e => e.Name == param))
+                {
+                    sw.WriteLine(item);
+                }
+            }
+        }
+        public static void Invoke(string name, string methodName)
+        {
+            try
+            {
+            Type type = Type.GetType(name, false, true);
+            object obj = Activator.CreateInstance(type);
+            string[] param = File.ReadAllLines(@"param.txt");
+            MethodInfo method = type.GetMethod(methodName);
+            method.Invoke(obj, param);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static object Create(string name)
+        {
+            return Activator.CreateInstance(Type.GetType(name));
+        }
+    }
+}
