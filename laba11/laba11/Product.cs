a27d4a041c838d938a275c3aa26c09788bd25b25
shinigamiiiii <75800331+shinigamiiiii@users.noreply.github.com>
2021-12-29 23:28:05 +0300

    Linq

---
+++ laba11/laba11/Product.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace laba11
+{
+    partial class Product
+    {
+        // fields
+        const int MAX_ID = 10; // const
+        static int id;
+        string name;
+        int upc;
+        string producer;
+        int price;
+        int bestBefore;
+        int count;
+
+        // constructors
+        public Product(string name, int upc, int price, int bestBefore, int count)
+        {
+            Console.WriteLine("Constructor");
+            if (!string.IsNullOrEmpty(name) && id <= MAX_ID)
+            {
+                this.name = name;
+                this.upc = upc;
+                this.price = price;
+                this.bestBefore = bestBefore;
+                this.count = count;
+                id++;
+            }
+            else
+            {
+                throw new Exception("Cannot create a product");
+            }
+        }
+        public Product()
+        {
+            Console.WriteLine("Constructor");
+            id++;
+        }
+        public Product(string name, int price, string producer, int upc = 11111, int bestBefore = 11111)
+        {
+            if (!string.IsNullOrEmpty(name) && id <= MAX_ID)
+            {
+                this.name = name;
+                this.producer = producer;
+                this.upc = upc;
+                this.price = price;
+                this.bestBefore = bestBefore;
+                count = 10;
+                id++;
+            }
+            else
+            {
+                throw new Exception("Cannot create a product");
+            }
+        }      
+
+        // getters and setters
+        public string Producer
+        {
+            get
+            {
+                return producer;
+            }
+            set
+            {
+                producer = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(name))
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new Exception("Name is null or empty");
+                }
+            }
+        }
+        public int Upc
+        {
+            get
+            {
+                return upc;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    upc = value;
+                }
+                else
+                {
+                    throw new Exception("Upc is incorrect");
+                }
+            }
+        }
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    throw new Exception("Price is incorrect");
+                }
+            }
+        }
+        public int BestBefore
+        {
+            get
+            {
+                return bestBefore;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    bestBefore = value;
+                }
+                else
+                {
+                    throw new Exception("BestBefore is incorrect");
+                }
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    count = value;
+                }
+                else
+                {
+                    throw new Exception("Count is incorrect");
+                }
+            }
+        }
+        //methods
+        public override string ToString()
+        {
+            return $"name: {Name}, price {Price}, producer {Producer}";
+        }
+    }
+}
+
---
+++ laba11/laba11/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace laba11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] months = {
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            };
+            Console.WriteLine("Enter length of month");
+            int n = Convert.ToInt32(Console.ReadLine());
+            IEnumerable<string> lengthN = from month in months
+                                          where month.Length == n
+                                          select month;
+            var winterSummer = months.Where((month, i) => i < 2 || i > 4 && i < 8 || i == 11);
+            var alphabetOrder = months.OrderBy(month => month);
+            var withU = months.Where(month => month.Contains("u") && month.Length > 3);
+
+            Show(lengthN);
+            Show(winterSummer);
+            Show(alphabetOrder);
+            Show(withU);
+
+            List<Product> products = new List<Product>() {
+                new Product("Tea Greenfield", 100, "Russia"),
+                new Product("Tea Jambo", 200, "Belarus"),
+                new Product("Tea Elephant", 50, "Belarus"),
+                new Product("Tea Lisma", 70, "India"),
+                new Product("Tea Java", 60, "Poland"),
+                new Product("Tea Tells", 150, "Ukrain"),
+                new Product("Tea Lipton", 170, "Russia"),
+                new Product("Sugar", 180, "ShriLanka")
+            };
+            var tea = products.Where(e => e.Name.Contains("Tea"));
+            var teaPrice = products.Where(e => e.Name.Contains("Tea") && e.Price < 200);
+            var count = products.Where(e => e.Price > 100).Count();
+            var maxPrice = products.Max(e => e.Price);
+            var producer = products.OrderBy(e => e.Producer);
+            var count1 = products.OrderBy(e => e.Price);
+
+            Console.WriteLine("-----------------");
+            foreach (var item in tea)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------");
+            foreach (var item in teaPrice)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------");
+            Console.WriteLine(count);
+            Console.WriteLine("-----------------");
+            Console.WriteLine(maxPrice);
+            Console.WriteLine("-----------------");
+            foreach (var item in producer)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------");
+            foreach (var item in count1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------");
+
+            var five = products.Where(e => e.Price != 200).Where(e => e.Producer != "Russia").OrderByDescending(e => e.Name).Select(e => e.Name += ';').Take(3);
+            Show(five);
+
+            List<Product> products1 = new List<Product>()
+            {
+                new Product("Coffee nescafe", 150, "Russia"),
+                new Product("Coffee jacobs", 200, "Belarus"),
+                new Product("Coffee Carte Noire", 50, "Belarus")
+            };
+            var samePrices = products.Join(
+                products1,
+                e => e.Price,
+                c => c.Price,
+                (c, e) => new { coffee = c.Name, tea = e.Name, price = c.Price });
+            Console.WriteLine("-----------------");
+            foreach (var item in samePrices)
+            {
+                Console.WriteLine($"price: ${item.price}\tcoffee: {item.coffee}\ttea: {item.tea}");
+            }
+        }
+        public static void Show(IEnumerable<string> str)
+        {
+            Console.WriteLine("-----------------");
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------");
+        }
+    }
+}
