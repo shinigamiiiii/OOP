1fb26490c06e194bdc00ddc4cb007189609c26da
shinigamiiiii <75800331+shinigamiiiii@users.noreply.github.com>
2021-12-29 23:20:24 +0300

    Наследование полиморфизм абстрактные интерфейсы

---
+++ laba5/laba5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Квитанция, Накладная, Документ, Чек, Дата, Организация. */
+namespace laba5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document docCheque = new Cheque(12122021, 10, 600);
+            Cheque cheque = docCheque as Cheque;
+            if (cheque != null)
+            {
+                cheque.SignADoc();
+                cheque.ShowInfo();
+            }
+            IOrganization orgWaybill = new Waybill(06102021, 400, 0.2);
+            if (orgWaybill is Waybill)
+            {
+                Waybill waybill1 = (Waybill)orgWaybill;
+                waybill1.ShowInfo();
+            }
+            Document docReceipt = new Receipt(07102021, 500, "organiz");
+            if (docReceipt is Receipt receipt)
+            {
+                receipt.SignADoc();
+                receipt.ShowInfo();
+            }
+            Console.WriteLine("------------------------------------");
+            Waybill waybill = orgWaybill as Waybill;
+            receipt = docReceipt as Receipt;
+            
+            Document[] docs = { cheque, waybill, receipt};
+            Printer printer = new Printer();
+            foreach (var item in docs)
+            {
+                printer.IAmPrinting(item);
+            }
+        }
+    }
+}
---
+++ laba5/laba5/Waybill.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba5
+{
+    class Waybill : Document, IOrganization
+    {
+        public double Fine
+        {
+            get;
+            set;
+        }
+        public Waybill(int date, int numberOfGoods, double fine ): base(date, numberOfGoods)
+        {
+            Fine = fine;
+        }
+        public bool SignADoc()
+        {
+            return Signed = true;
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Переопределенный метод showinfo\nНалог: " + Fine);
+        }
+        public override string ToString()
+        {
+            return $"Класс Waybill";
+        }
+    }
+}
